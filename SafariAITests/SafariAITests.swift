3cf9c9b9356d1d61c59e440e77cf68912693d492
Vinayak Kumar <kumarvinayak829@gmail.com>
2024-11-28 23:24:17 +0530

    Initial Commit

---
+++ SafariAITests/SafariAITests.swift
@@ -0,0 +1,17 @@
+//
+//  SafariAITests.swift
+//  SafariAITests
+//
+//  Created by Vinayak Kumar on 28/11/24.
+//
+
+import Testing
+@testable import SafariAI
+
+struct SafariAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
